807a393b68b8108f8b931daf77f9d730c30e9c3f
Sourav Shetye <143427277+Sourav0211@users.noreply.github.com>
2025-07-20 11:30:25 -0400

    Time: 0 ms (100.00%) | Memory: 10 MB (67.42%) - LeetSync

--- 567-permutation-in-string/permutation-in-string.cpp
+++ 567-permutation-in-string/permutation-in-string.cpp
@@ -1,42 +1,29 @@
 class Solution {
 public:
     bool checkInclusion(string s1, string s2) {
-        if (s1.length() > s2.length()) return false;
+        if (s2.length() < s1.length()) return false;
 
-        vector<int> s1Freq(26, 0), windowFreq(26, 0);
+        vector<int> s1Freq(26,0);
+        vector<int> s2Freq(26,0);
 
-        for (char c : s1) s1Freq[c - 'a']++;
-
-        int matchCount = 0;
-        int requiredMatches = 26;
-
-        // Preprocess the first window
-        for (int i = 0; i < s1.length(); ++i) {
-            windowFreq[s2[i] - 'a']++;
+        for(char ch: s1)
+        {
+            s1Freq[ch - 'a']++;
         }
 
-        for (int i = 0; i < 26; ++i) {
-            if (s1Freq[i] == windowFreq[i]) matchCount++;
+        for(int i = 0;i<s1.length();i++)
+        {
+            s2Freq[s2[i] - 'a']++;
         }
+        if(s1Freq ==s2Freq) return true;
 
-        if (matchCount == requiredMatches) return true;
+        for(int k = s1.length(); k<s2.length();k++)
+        {
+            s2Freq[s2[k] - 'a']++;
 
-        // Slide window
-        for (int i = s1.length(); i < s2.length(); ++i) {
-            int add = s2[i] - 'a';
-            int remove = s2[i - s1.length()] - 'a';
+            s2Freq[ s2[k - s1.length()]- 'a']--;
 
-            // Handle removing left char
-            if (s1Freq[remove] == windowFreq[remove]) matchCount--;
-            windowFreq[remove]--;
-            if (s1Freq[remove] == windowFreq[remove]) matchCount++;
-
-            // Handle adding right char
-            if (s1Freq[add] == windowFreq[add]) matchCount--;
-            windowFreq[add]++;
-            if (s1Freq[add] == windowFreq[add]) matchCount++;
-
-            if (matchCount == requiredMatches) return true;
+            if(s1Freq == s2Freq) return true;
         }
 
         return false;
